29a3ad635cce23c930f4ed8d018edf334e58365a
shivani panwar <shivanipanwar5242@gmail.com>
2020-09-28 09:47:30 +0530

    Username and Password

---
+++ app.js
@@ -0,0 +1,87 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true,useUnifiedTopology:true});
+const userSchema=new mongoose.Schema({
+    email:String,
+    password:String
+});
+const User=mongoose.model("User",userSchema);
+
+
+app.get("/", function(req,res){
+    res.render("home");
+})
+app.get("/login", function(req,res){
+    res.render("login");
+})
+app.get("/register", function(req,res){
+
+    res.render("register",{err:null});
+})
+app.post("/register", function(req,res){
+
+
+const newUser=new User({
+email:req.body.username,
+password:req.body.password
+})
+
+newUser.save(function(err){
+    if(!err){
+res.render("secrets");
+    }
+    else{
+        console.log(err);
+    }
+});
+
+
+});
+
+app.post("/login", function(req,res){
+
+const eamil=req.body.username;
+const password=req.body.password;
+User.findOne({email:req.body.username, password:req.body.password}, function(err, foundUsers){
+    if(!foundUsers){
+      
+        res.render("register", {err:"OOPs you don't have an account"});
+      
+    }
+    else{
+        res.render("secrets");
+    }
+})
+   
+  
+
+
+});
+    
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+  
